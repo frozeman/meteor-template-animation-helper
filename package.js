e30452a7d7bc18e78b727b5ff1aae0451d2714ed
Fabian Vogelsteller <fabian@frozeman.de>
2014-08-27 13:58:06 +0200

    made 0.9.0 ready

--- package.js
+++ package.js
@@ -1,20 +1,26 @@
 Package.describe({
-    summary: "Allows to animate templates by adding a 'animate' class with css transitions"
+    name: "mrt:template-animation-helper",
+    summary: "Allows to animate templates by adding a 'animate' class with css transitions",
+    version: "1.1.6",
+    git: "https://github.com/frozeman/meteor-template-animation-helper.git"
 });
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
+    api.versionsFrom('METEOR@0.9.0');
+
+    // core
     api.use('underscore', 'client');
     api.use('templating', 'client');
     api.use('session', 'client');
     api.use('jquery', 'client');
 
     // thirdparty
-    api.use('view-manager', 'client', {weak: true});
+    api.use('mrt:view-manager@0.1.6', 'client', {weak: true});
 
     // EXPORT
     api.export('AnimateTemplate');
 
     // FILES
-    api.add_files('animationTemplate.html', 'client');
-    api.add_files('animationTemplate.js', 'client');
+    api.addFiles('animationTemplate.html', 'client');
+    api.addFiles('animationTemplate.js', 'client');
 });
